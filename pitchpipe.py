1ba7f735b14ec3bcd8d44c21f0abe1f836715786
Floris van Vugt <f.t.vanvugt@gmail.com>
2016-12-01 11:11:19 +0100

    Minor fix.

--- pitchpipe.py
+++ pitchpipe.py
@@ -16,36 +16,7 @@ import pickle
 
 from temperament import *
 
-#pitches = "c c# d eb e f f# g g# a bb b".split(" ")
-flat  = chr(0x266D) #unichr(0x266D)
-sharp = chr(0x266F) #unichr(0x266F)
-#pitchlist = "a bes b c cis d ees e f fis g gis"
-#pitchlist = pitchlist.replace('is',sharp).replace('es',flat)
-#pitches = pitchlist.split(" ")
-basepitch = 415
-
 temperaments = [ "quarter-comma meantone", "equal" ]
-
-
-# For each note in the 12-note scale, this encodes how many fifths it is away
-# from the tonic, first note in the scale. 
-fifth_distance = np.array([ 0, -5, 2, -3, 4, -1, 6, 1, -4, 3, -2, 5 ])
-# Similarly, for each note in the scale, this encodes the "octave correction" that makes
-# it end up in the right place relative to the starting pitch.
-sec_pow        = np.array([ 0, 3, -1, 2, -2, 1, -3, 0, 3, -1, 2, -2 ])
-temp_perf_fifth = np.power(5,.25)
-
-# When you look at this, it makes sense: https://en.wikipedia.org/wiki/Quarter-comma_meantone
-# Also, some cool sound examples https://en.wikipedia.org/wiki/Just_intonation
-
-def get_freq_ratios(temperament):
-    if temperament == "quarter-comma meantone":
-        return np.power(temp_perf_fifth,fifth_distance)*np.power(2.,sec_pow)
-    if temperament == "equal":
-        # Equal temperament
-        return np.power(2.,np.arange(0,12)/12.)
-        
-
 
 
 
@@ -223,9 +194,9 @@ class Frame(wx.Frame):
         self.octcorr.Bind( wx.EVT_TEXT, self.textChange)
         octp.Add( self.octcorr )
         self.incrb = wx.Button(panel,  wx.ID_ADD,"+",size=(25,-1))
-        self.incrb.Bind(wx.EVT_BUTTON, self.onRootChange)
+        self.incrb.Bind(wx.EVT_BUTTON, self.onOctaveChange)
         self.decrb = wx.Button(panel, wx.ID_DELETE, "-",size=(25,-1))
-        self.decrb.Bind(wx.EVT_BUTTON,  self.onRootChange)
+        self.decrb.Bind(wx.EVT_BUTTON,  self.onOctaveChange)
         octp.Add( self.decrb )
         octp.Add( self.incrb )
         rootp.Add(octp)
@@ -428,6 +399,7 @@ class Frame(wx.Frame):
 
         self.octcorr.SetValue(str(octcorr))
 
+        self.update_from_root()
         self.update_freqrat()
         self.update_pitch()
 
